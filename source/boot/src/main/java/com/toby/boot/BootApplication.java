ede21101b26edc01336b7ec3135133c464d8dec8
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-02-18 23:20:00 +0900

    1. 서블릿 컨테이너 만들기 1) 스프링 부트 주석 제거 2) 톰캣 생성 3) 실행

--- source/boot/src/main/java/com/toby/boot/BootApplication.java
+++ source/boot/src/main/java/com/toby/boot/BootApplication.java
@@ -1,13 +1,23 @@
 package com.toby.boot;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
+//		SpringApplication.run(BootApplication.class, args);
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+		// servlet 컨테이너를 만든다
+		WebServer webServer = serverFactory.getWebServer();
+
+		webServer.start();
 	}
 
 }
