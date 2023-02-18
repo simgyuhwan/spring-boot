155482d5409a19486698f2857726a03b26a9ab14
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-02-18 23:33:58 +0900

    1. 서블릿 컨테이너 만들기 1) 서블릿 추가

--- source/boot/src/main/java/com/toby/boot/BootApplication.java
+++ source/boot/src/main/java/com/toby/boot/BootApplication.java
@@ -1,11 +1,19 @@
 package com.toby.boot;
 
 //import org.springframework.boot.SpringApplication;
+import com.toby.boot.controller.IndexController;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 //@SpringBootApplication
 public class BootApplication {
@@ -15,9 +23,18 @@ public class BootApplication {
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
 		// servlet 컨테이너를 만든다
-		WebServer webServer = serverFactory.getWebServer();
+		WebServer webServer = serverFactory.getWebServer(servletContext ->
+				servletContext.addServlet("hello", new HttpServlet() {
+					@Override
+					protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+						res.setStatus(HttpStatus.OK.value());
+						res.setHeader("Content-Type", "text/plain");
+						res.getWriter().println("Hello Servlet");
+						super.service(req, res);
+					}
+				}).addMapping("/hello")
+		);
 
 		webServer.start();
 	}
-
 }
