839bcb6bfb0169482a0331dba3c9fb05c563bf53
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-02-19 00:09:56 +0900

    1. 서블릿 컨테이너 만들기 1) 컨텐츠 생성 책임 분리

--- source/boot/src/main/java/com/toby/boot/BootApplication.java
+++ source/boot/src/main/java/com/toby/boot/BootApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -21,24 +22,29 @@ import java.io.IOException;
 public class BootApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(BootApplication.class, args);
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
-		// servlet 컨테이너를 만든다
+		IndexController indexController = new IndexController();
 		WebServer webServer = serverFactory.getWebServer(servletContext ->
-				servletContext.addServlet("hello", new HttpServlet() {
+				servletContext.addServlet("frontcontroller", new HttpServlet() {
 					@Override
 					protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-						// 요청 값 추출
-						String name = req.getParameter("name");
+						if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+							String name = req.getParameter("name");
 
-						// 응답 설정
-						res.setStatus(HttpStatus.OK.value());
-						res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-						res.getWriter().println("Hello " + name);
-						super.service(req, res);
+							// 응답 값의 책임 분리
+							String ret = indexController.hello(name);
+
+							res.setStatus(HttpStatus.OK.value());
+							res.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+							res.getWriter().println(ret);
+						} else if(req.getRequestURI().equals("/user")) {
+
+						} else {
+							res.setStatus(HttpStatus.NOT_FOUND.value());
+						}
 					}
-				}).addMapping("/hello")
+				}).addMapping("/*")
 		);
 
 		webServer.start();
--- source/boot/src/main/java/com/toby/boot/controller/IndexController.java
+++ source/boot/src/main/java/com/toby/boot/controller/IndexController.java
@@ -1,12 +1,6 @@
 package com.toby.boot.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class IndexController {
-
-    @GetMapping("/hello")
     public String hello(String name) {
         return "Hello " + name;
     }
