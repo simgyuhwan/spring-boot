23793540979e068dd4b7cd5525b33c4ca66ad6ee
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-02-20 01:08:05 +0900

    1. 스프링 컨테이너 만들기 1) 사용안하는 주석 제거

--- source/boot/src/main/java/com/toby/boot/BootApplication.java
+++ source/boot/src/main/java/com/toby/boot/BootApplication.java
@@ -1,30 +1,20 @@
 package com.toby.boot;
 
 //import org.springframework.boot.SpringApplication;
+
 import com.toby.boot.controller.IndexController;
 import com.toby.boot.service.SimpleIndexService;
-import org.apache.catalina.startup.Tomcat;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 //@SpringBootApplication
 public class BootApplication {
 
 	public static void main(String[] args) {
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
 		applicationContext.registerBean(IndexController.class);
 		// DI를 위한 구현체 빈 등록
 		applicationContext.registerBean(SimpleIndexService.class);
@@ -33,22 +23,9 @@ public class BootApplication {
 
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext ->
-				servletContext.addServlet("frontcontroller", new HttpServlet() {
-					@Override
-					protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-						if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-							String name = req.getParameter("name");
-
-							IndexController indexController = applicationContext.getBean(IndexController.class);
-							String ret = indexController.hello(name);
-
-							res.setContentType(MediaType.TEXT_PLAIN_VALUE);
-							res.getWriter().println(ret);
-						} else {
-							res.setStatus(HttpStatus.NOT_FOUND.value());
-						}
-					}
-				}).addMapping("/*")
+				servletContext.addServlet("dispatcherServlet",
+					new DispatcherServlet(applicationContext)
+				).addMapping("/*")
 		);
 
 		webServer.start();
