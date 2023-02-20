510d88b457dfca4074049551d76e606d6752e52c
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-02-20 21:27:16 +0900

    1. 빈 등록 1) Configuration 생성 및 등록

--- source/boot/src/main/java/com/toby/boot/BootApplication.java
+++ source/boot/src/main/java/com/toby/boot/BootApplication.java
@@ -3,31 +3,50 @@ package com.toby.boot;
 //import org.springframework.boot.SpringApplication;
 
 import com.toby.boot.controller.IndexController;
+import com.toby.boot.service.IndexService;
 import com.toby.boot.service.SimpleIndexService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-//@SpringBootApplication
+@Configuration
 public class BootApplication {
 
+	@Bean
+	public IndexController indexController(IndexService indexService) {
+		return new IndexController(indexService);
+	}
+
+	@Bean
+	public IndexService indexService() {
+		return new SimpleIndexService();
+	}
+
 	public static void main(String[] args) {
-		GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-		applicationContext.registerBean(IndexController.class);
-		// DI를 위한 구현체 빈 등록
-		applicationContext.registerBean(SimpleIndexService.class);
+		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+			@Override
+			protected void onRefresh() {
+				super.onRefresh();
 
+				ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+				WebServer webServer = serverFactory.getWebServer(servletContext ->
+						servletContext.addServlet("dispatcherServlet",
+								new DispatcherServlet(this)
+						).addMapping("/*")
+				);
+				webServer.start();
+			}
+		};
+
+		// Configuration 이 붙은 구성 클래스를 등록
+		applicationContext.register(BootApplication.class);
 		applicationContext.refresh();
-
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext ->
-				servletContext.addServlet("dispatcherServlet",
-					new DispatcherServlet(applicationContext)
-				).addMapping("/*")
-		);
-
-		webServer.start();
 	}
 }
--- source/boot/src/main/java/com/toby/boot/controller/IndexController.java
+++ source/boot/src/main/java/com/toby/boot/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.toby.boot.controller;
 
 import com.toby.boot.service.IndexService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
+@RequestMapping
 public class IndexController {
     private final IndexService indexService;
 
@@ -11,6 +16,8 @@ public class IndexController {
         this.indexService = indexService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         return indexService.sayHello(Objects.requireNonNull(name));
     }
