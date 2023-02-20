d8c825a5e15ea41976705457ceb917b6710b65d2
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-02-20 22:09:09 +0900

    1. 빈 등록 1) real final

--- source/boot/src/main/java/com/toby/boot/BootApplication.java
+++ source/boot/src/main/java/com/toby/boot/BootApplication.java
@@ -1,21 +1,11 @@
 package com.toby.boot;
 
-//import org.springframework.boot.SpringApplication;
-
-import com.toby.boot.controller.IndexController;
-import com.toby.boot.service.IndexService;
-import com.toby.boot.service.SimpleIndexService;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -33,24 +23,7 @@ public class BootApplication {
 	}
 
 	public static void main(String[] args) {
-		AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-			@Override
-			protected void onRefresh() {
-				super.onRefresh();
-
-				ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-				DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-				WebServer webServer = serverFactory.getWebServer(servletContext ->
-						servletContext.addServlet("dispatcherServlet", dispatcherServlet
-						).addMapping("/*")
-				);
-				webServer.start();
-			}
-		};
-
-		// Configuration 이 붙은 구성 클래스를 등록
-		applicationContext.register(BootApplication.class);
-		applicationContext.refresh();
+		SpringApplication.run(BootApplication.class, args);
 	}
+
 }
