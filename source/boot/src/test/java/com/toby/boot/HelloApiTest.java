715c2c2ae0c784e71e85a74d495297653d490bcb
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-02-21 23:08:58 +0900

    1. 테스트 코드 1) API 성공 테스트

---
+++ source/boot/src/test/java/com/toby/boot/HelloApiTest.java
@@ -0,0 +1,24 @@
+package com.toby.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+
+        ResponseEntity<String> res
+                = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Sim");
+
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(res.getHeaders().getContentType().includes(MediaType.TEXT_PLAIN)).isTrue();
+        assertThat(res.getBody()).isEqualTo("Hello Sim");
+    }
+}
