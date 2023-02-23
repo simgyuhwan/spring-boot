d94121f7cdf24787f954c7972a6f60246cad6536
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-02-24 00:11:44 +0900

    1. 자동 구성 1) 파일 로더 구성

--- source/boot/src/main/java/com/toby/config/autoconfig/DispatcherServletConfig.java
+++ source/boot/src/main/java/com/toby/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package com.toby.config.autoconfig;
 
+import com.toby.config.MyAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet() {
