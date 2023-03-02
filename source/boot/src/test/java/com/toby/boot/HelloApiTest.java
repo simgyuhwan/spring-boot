870083134b4fe1d2c089dec27bb9a5d4fe48f299
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-03-02 22:29:33 +0900

    1. 자동 구성 1) ConditionalOnMissingBean 사용 2) 메인 클래스 위치 이동.

--- source/boot/src/test/java/com/toby/boot/HelloApiTest.java
+++ source/boot/src/test/java/com/toby/boot/HelloApiTest.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 public class HelloApiTest {
     @Test
     void helloApi() {
