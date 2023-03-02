d0a200b7e4d1ac656fba0979b53334afc828c463
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-03-03 00:19:11 +0900

    1. 외부 설정을 이용한 자동 구성 1) value property placeholder 적용

---
+++ source/boot/src/main/java/com/toby/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,13 @@
+package com.toby.config.autoconfig;
+
+import com.toby.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
--- source/boot/src/main/java/com/toby/config/autoconfig/TomcatWebServerConfig.java
+++ source/boot/src/main/java/com/toby/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.toby.config.autoconfig;
 
 import com.toby.boot.annotation.ConditionalMyOnClass;
 import com.toby.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -11,11 +12,15 @@ import org.springframework.core.env.Environment;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+
+    @Value("${contextPath}")
+    private String contextPath;
+
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory(Environment env) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 
