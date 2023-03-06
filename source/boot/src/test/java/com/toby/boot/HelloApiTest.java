0cf87235027bc1939c55136ec1c7811515d9b702
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-03-06 23:52:36 +0900

    1. 외부 설정을 이용한 자동 구성 1) Environment 사용3

--- source/boot/src/test/java/com/toby/boot/HelloApiTest.java
+++ source/boot/src/test/java/com/toby/boot/HelloApiTest.java
@@ -1,11 +1,9 @@
 package com.toby.boot;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-
 import static org.assertj.core.api.Assertions.assertThat;
 public class HelloApiTest {
     @Test
